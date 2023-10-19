7b84d00fbbc4cbff823d99a02baa2f555f37d40e
Manoj Kanaka DH <134923961+manojkanaka24@users.noreply.github.com>
2023-10-19 15:55:00 +0530

    Day 2 : - Operators
    On my second day of practice, I revisited the "Operator Insights" repository to deepen my grasp of programming operators.
    
    Today, I explored more advanced concepts, honing my skills in the process.
    
    I delved into Bitwise Operators, gaining a better understanding of how they manipulate binary data. It felt like unlocking a new level of control in my code.
    
    Next, I learned about the Conditional Operator. Its concise syntax made decision-making in my code much smoother and efficient.
    
    In JavaScript, I got acquainted with the typeof Operator. It helped me understand the types of variables, a crucial insight for writing robust code.
    
    Then, I dived into instanceof Operator, which proved incredibly handy for working with complex data structures.
    
    Finally, I learned about the in Operator, which opened up new ways to explore object properties.
    
    Looking back, it's amazing how much progress I've made in just two days. "Operator Insights" has been a valuable companion in my journey to master programming operators. I'm excited to see what tomorrow's practice will bring.

---
+++ operators.java
@@ -0,0 +1,16 @@
+public class operators {
+    public static void main(String args[]){
+        int a = 2;
+        int c = ++a ;
+        System.out.println(a);
+        System.out.println(c);
+
+
+
+        int b = 5;
+         b *= 5;
+        System.out.println(b);
+       
+    }
+    
+}
